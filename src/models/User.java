d0f585f5c310787204d169509b7110a343f0a55b
20070590 <20070590@IT118PC19.wit.ie>
2017-10-06 14:08:36 +0100

    getUsers now returns a Collection Instead of a List.

--- src/models/User.java
+++ src/models/User.java
@@ -1,6 +1,7 @@
 package models;
 
 import static com.google.common.base.MoreObjects.toStringHelper;
+import com.google.common.base.Objects;
 
 public class User {
 
@@ -21,13 +22,20 @@ public class User {
 		this.password = password;
 	}
 	
-	 public String toString()
+	  @Override
+	  public String toString()
 	  {
 	    return toStringHelper(this).addValue(firstName)
 	                               .addValue(lastName)
 	                               .addValue(password)
 	                               .addValue(email)                               
 	                               .toString();
+	  }
+	 
+	  @Override  
+	  public int hashCode()  
+	  {  
+	     return Objects.hashCode(this.lastName, this.firstName, this.email, this.password);  
 	  }
 }
 
